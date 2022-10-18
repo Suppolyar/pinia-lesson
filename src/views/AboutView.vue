61981d7fb757e216ab8e3773d521d05cc8bf4fdf
Ilya Zakharov <74999345+Suppolyar@users.noreply.github.com>
2022-10-18 16:16:46 +0600

    example of Pinia work

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {useCounterStore} from "@/stores/counter";
+
+const counterStore = useCounterStore()
+</script>
+
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <button @click="counterStore.increaseCount" class="counter-btn">{{counterStore.count}}</button>
+  </div>
+</template>
+
+<style>
+.counter-btn {
+  font-size: 60px;
+  margin: 20px;
+}
+</style>
